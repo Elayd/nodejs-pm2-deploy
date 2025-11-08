1eef12b196c2bd354c4b69da17b3c858d5865ce1
Elayd <elaydweb@gmail.com>
2025-11-08 17:21:39 +0300

    fix env params

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '../.env.deploy' });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO = 'origin/master',
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master', DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
